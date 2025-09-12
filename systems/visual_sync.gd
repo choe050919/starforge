505c5e3c9713b1388b7e8a6a9e5c7c0664f7303d
choe050919 <choe050919@gmail.com>
2025-09-12 15:00:43 +0900

    VisualSync의 _refresh_indices 함수를, Ground에 동기화 필요.
    0.0.10.9

--- systems/visual_sync.gd
+++ systems/visual_sync.gd
@@ -4,7 +4,7 @@ class_name VisualSync
 var _data_layer: DataLayer
 var substance: SubstanceStore
 var phase: PhaseStore
-var mass
+var mass: MassStore
 var temp: TemperatureStore
 
 @export var heatmap_overlay: HeatmapOverlay
@@ -13,6 +13,7 @@ func setup(data_layer: DataLayer) -> void:
 	_data_layer = data_layer
 	substance = _data_layer.substance
 	phase = _data_layer.phase
+	mass = _data_layer.mass
 	temp = _data_layer.temperature
 
 ## 본체
@@ -26,22 +27,20 @@ func _on_tiles_changed(
 ) -> void:
 	# 1) 전체 무효화 신호면 풀 리프레시
 	if payload.get("full_refresh", false):
-		print("all")
 		_refresh_all(payload) # ← 내부에서 각 레이어/텍스처 전체 재생성
 		return
 
 	# 2) 부분 업데이트 경로
 	if idxs.is_empty():
-		push_error("[VisualSync] not full_refresh and idxs is empty");
-		return
-		print("something")
+		push_error("[VisualSync] not full_refresh and idxs is empty"); return
+		print("something") # DEBUG HACK
 	_refresh_indices(idxs, payload)
 
 func _refresh_all(payload: Dictionary) -> void:
-	var ch_sid  : bool = payload.get("sid_changed", false)
-	var ch_phase: bool = payload.get("phase_changed", false)
-	var ch_mass : bool = payload.get("mass_changed", false)
-	var ch_temp : bool = payload.get("temp_changed", false)
+	var ch_sid   : bool = payload.get("sid_changed", false)
+	var ch_phase : bool = payload.get("phase_changed", false)
+	var ch_mass  : bool = payload.get("mass_changed", false)
+	var ch_temp  : bool = payload.get("temp_changed", false)
 
 	if ch_temp:
 		heatmap_overlay.render_full_with_mask(temp.get_raw_read(), phase.get_raw_read())
@@ -53,7 +52,33 @@ func _refresh_all(payload: Dictionary) -> void:
 	# ... 필요 노드만 호출
 
 func _refresh_indices(idxs: PackedInt32Array, payload: Dictionary) -> void:
-	pass
+	var ch_sid   : bool = payload.get("sid_changed", false)
+	var ch_phase : bool = payload.get("phase_changed", false)
+	var ch_mass  : bool = payload.get("mass_changed", false)
+	var ch_temp  : bool = payload.get("temp_changed", false)
+
+	for i in idxs:
+		var cell := _data_layer.index.cell(i)
+
+		## ── Substance/Phase 변경 → Terrain 쪽 갱신
+		#if ch_sid or ch_phase:
+			#var sid   := substance.get_by_index(i)
+			#var phase := phase.get_by_index(i)
+			#_terrain.update_tile(cell, sid, phase)
+#
+		## ── Mass 변경 → 액체 오버레이
+		#if ch_mass:
+			#var m := mass.get_by_index(i)
+			#_lo.update_cell(cell, m)
+#
+		## ── Temperature 변경 → 히트맵 오버레이
+		#if ch_temp:
+			#var t := temp.get_by_index(i)
+			#_lo.update_heat(cell, t)
+
+
+
+
 
 @export_node_path("Node") var terrain_path: NodePath
 @export_node_path("Node") var liquid_overlay_path: NodePath
