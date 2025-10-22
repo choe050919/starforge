4b101223852201abf42837459b6261c00ee22212
choe050919 <choe050919@gmail.com>
2025-10-22 22:33:34 +0900

    Debug 유틸리티에 warn/error 함수 추가 및 리팩터링
    - warn(), error() 함수 추가 (push_warning/push_error 사용)
    - warn/error는 debug_enabled 체크 없이 항상 출력
    - 태그 생성 및 메시지 포맷팅 로직을 내부 헬퍼로 분리
    - 클래스 도큐먼트 주석 추가

--- autoload/debug.gd
+++ autoload/debug.gd
@@ -1,4 +1,8 @@
 extends Node
+## 디버그 메시지 출력을 위한 전역 유틸리티.
+## 
+## log(), warn(), error() 함수를 제공하며,
+## 호출자 객체의 클래스명/스크립트명을 자동으로 태그에 포함합니다.
 
 ## 디버그 로그를 출력합니다.
 ## 
@@ -11,14 +15,45 @@ func log(caller: Object, msg: String, args: Array = []) -> void:
 	if not caller.get("debug_enabled"):
 		return
 	
-	var tag: String
+	var tag := _get_tag(caller)
+	var formatted_msg := _format_message(tag, msg, args)
+	print(formatted_msg)
+
+## 경고 메시지를 출력합니다.
+## 
+## 사용법:
+##   Debug.warn(self, "메시지")
+##   Debug.warn(self, "포맷: %d, %s", [123, "테스트"])
+## 
+## debug_enabled와 무관하게 항상 출력됩니다.
+func warn(caller: Object, msg: String, args: Array = []) -> void:
+	var tag := _get_tag(caller)
+	var formatted_msg := _format_message(tag, msg, args)
+	push_warning(formatted_msg)
+
+## 에러 메시지를 출력합니다.
+## 
+## 사용법:
+##   Debug.error(self, "메시지")
+##   Debug.error(self, "포맷: %d, %s", [123, "테스트"])
+## 
+## debug_enabled와 무관하게 항상 출력됩니다.
+func error(caller: Object, msg: String, args: Array = []) -> void:
+	var tag := _get_tag(caller)
+	var formatted_msg := _format_message(tag, msg, args)
+	push_error(formatted_msg)
+
+## caller 객체로부터 태그를 생성합니다.
+func _get_tag(caller: Object) -> String:
 	var script = caller.get_script()
 	if script and script.get_global_name():
-		tag = script.get_global_name()
+		return script.get_global_name()
 	else:
-		tag = caller.get_class()
-	
+		return caller.get_class()
+
+## 메시지를 포맷팅합니다.
+func _format_message(tag: String, msg: String, args: Array) -> String:
 	if args.is_empty():
-		print("[%s] %s" % [tag, msg])
+		return "[%s] %s" % [tag, msg]
 	else:
-		print("[%s] %s" % [tag, msg % args])
+		return "[%s] %s" % [tag, msg % args]
